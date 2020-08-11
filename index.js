6228e4523b0f5913256c0e72788b981f35ff050a
Mikael Tellhed <mikael.tellhed@gmail.com>
2020-08-11 15:10:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,149 @@
+Noodl.defineNode = function(def) {
+    const _def = {};
+    const _outputs = {};
+
+    _def.name = def.package + '-' + def.name;
+    _def.displayNodeName = def.displayNodeName || def.name;
+    _def.color = def.color || 'default';
+    _def.category = def.category || def.package || 'Modules';
+    _def.initialize = function() {
+        this.inputs = {};
+        var _this = this;
+        this.setOutputs = function(o) {
+            for(var key in o) {
+                _outputs[key] = o[key];
+                _this.flagOutputDirty(key);
+            }
+        }
+        if(typeof def.initialize === 'function')
+            def.initialize.apply(this);
+    }
+    _def.inputs = {};
+    _def.outputs = {};
+
+    for(var key in def.inputs) {
+        _def.inputs[key] = {
+            type:(typeof def.inputs[key] === 'object')?def.inputs[key].type:def.inputs[key],
+            displayName:(typeof def.inputs[key] === 'object')?def.inputs[key].displayName:undefined,
+            group:(typeof def.inputs[key] === 'object')?def.inputs[key].group:undefined,
+            default:(typeof def.inputs[key] === 'object')?def.inputs[key].default:undefined,
+            set:(function() { const _key = key; return function(value) {
+                this.inputs[_key] = value;
+                if(def.changed && typeof def.changed[_key] === 'function') {
+                    def.changed[_key].apply(this,[value]);
+                }
+            }})()
+        }
+    }
+
+    for(var key in def.signals) {
+        _def.inputs[key] = {
+            type:'signal',
+            valueChangedToTrue:(function() { const _key = key; return function() {
+                if(typeof def.signals[_key] === 'function') {
+                    this.scheduleAfterInputsHaveUpdated(() => {
+                        def.signals[_key].apply(this);
+                    }) 
+                }
+            }})()
+        }
+    }
+
+    for(var key in def.outputs) {
+        if(def.outputs[key] === 'signal') {
+            _def.outputs[key] = {
+                type:'signal',
+            }
+        }
+        else {
+            _def.outputs[key] = {
+                type:def.outputs[key],
+                getter:(function(_key) {
+                    return _outputs[_key];
+                }).bind(this,key)
+            }
+        }
+    }
+
+    _def.prototypeExtensions = {};
+    for(var key in def.methods) {
+        _def.prototypeExtensions[key] = def.methods[key];
+    }
+
+    return {node:_def,setup:def.setup};
+}
+
+Noodl.defineCollectionNode = function(def) {
+    const _def = {
+        name:def.name,
+        package:def.package,
+        color:'data',
+        inputs:def.inputs,
+        outputs:Object.assign({
+            Items:'array',
+            'Fetch Started':'signal',
+            'Fetch Completed':'signal'
+        },def.outputs||{}),
+        signals:Object.assign({
+            Fetch:function() {
+                var _this = this;
+                this.sendSignalOnOutput('Fetch Started');
+                var a = def.fetch.call(this,function() {
+                    _this.sendSignalOnOutput('Fetch Completed');
+                });
+                this.setOutputs({
+                    Items:a
+                })
+            }
+        },def.signals||{})
+    }
+
+    return Noodl.defineNode(_def);
+}
+
+Noodl.defineModelNode = function(def) {
+    const _def = {
+        name:def.name,
+        package:def.package,
+        color:'data',
+        inputs:{
+            Id:'string'
+        },
+        outputs:{
+            Fetched:'signal'
+        },
+        changed:{
+            Id:function(value) {
+                if(this._object && this._changeListener)
+                    this._object.off('change',this._changeListener)
+                
+                this._object = Noodl.Object.get(value);
+                this._changeListener = (name,value) => {
+                    const _o = {}
+                    _o[name] = value;
+                    this.setOutputs(_o)
+                }
+                this._object.on('change',this._changeListener)
+
+                this.setOutputs(this._object.data);
+                this.sendSignalOnOutput('Fetched');
+            }
+        },
+        initialize:function() {
+
+        }
+    }
+
+    for(var key in def.properties) {
+        _def.inputs[key] = def.properties[key];
+        _def.outputs[key] = def.properties[key];
+        _def.changed[key] = (function() { const _key = key; return function(value) {
+            if(!this._object) return;
+            this._object.set(_key,value);
+        }})()
+    }
+ 
+    return Noodl.defineNode(_def);
+}
+
+module.exports = Noodl;
